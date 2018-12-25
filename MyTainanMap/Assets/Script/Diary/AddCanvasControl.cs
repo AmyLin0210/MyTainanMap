bdf7ab79c9128bb30af539ddc4d25e99b77a3435
林謙慈 <amylin19990210@gmail.com>
2018-12-25 17:53:33 +0800

    add new canvas

---
+++ MyTainanMap/Assets/Script/Diary/AddCanvasControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCanvasControl : MonoBehaviour {
+
+    string diary_path, build_path;
+    public void ShowCanvas( string path)
+    {
+        build_path = path;
+        gameObject.GetComponent<Canvas>().enabled = true;
+    }
+}
--- MyTainanMap/Assets/Script/Diary/CanvasDiaryControl.cs
+++ MyTainanMap/Assets/Script/Diary/CanvasDiaryControl.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class CanvasDiaryControl : MonoBehaviour {
 
-    public Canvas canvas_add;
+    public GameObject diary_info;
+    public GameObject canvas_add;
     public Canvas canvas_new;
-    public Canvas canvas_read;
+    public GameObject canvas_read;
+    string building_path, diar_path, project_path;
 
-    void OpenDiary( string file_path )
+    bool isOpenDiary;
+
+    void Start()
     {
-
+        project_path = diary_info.GetComponent<DiaryInfo>().DirectoryPath;
     }
+
+    void OpenDiary( string path )
+    {
+        bool isDiary = false;
+        string fileConetent;
+        StreamReader sr;
+        building_path = path;
+
+        sr = new StreamReader(building_path);
+
+        while( (fileConetent = sr.ReadLine()) != null)
+        {
+            if(fileConetent == "diary")
+            {
+                isDiary = true;
+                break;
+            }
+        }
+
+        // open read diary canvas
+        if (isDiary)
+            canvas_read.GetComponent<AddCanvasControl>().ShowCanvas(building_path);
+        // open add diary canvas
+        else
+            canvas_add.GetComponent<AddCanvasControl>().ShowCanvas(building_path);
+    }
+
+
 }
